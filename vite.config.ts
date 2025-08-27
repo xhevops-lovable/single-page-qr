5ddd2d14d14dca6f8ecb8fc42b187fbbb43d8f0d
xhevo <xhevops@gmail.com>
2025-08-27 12:12:18 +0200

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/single-page-qr/",
   server: {
     host: "::",
     port: 8080,
